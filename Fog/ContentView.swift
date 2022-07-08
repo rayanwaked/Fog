c7cacd1e845fdda39b24ed1cbb913a7d2c367e0b
rayan <87000151+rayanwaked@users.noreply.github.com>
2022-07-07 22:29:01 -0700

    Initial Commit

---
+++ Fog/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Fog
+//
+//  Created by Rayan Waked on 7/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fog/FogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FogApp.swift
+//  Fog
+//
+//  Created by Rayan Waked on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
